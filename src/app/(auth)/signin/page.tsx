296b07ca309fc3a05bbb0579aea261cc6f94619f
shivaacodes <shivasajay007@gmail.com>
2024-12-29 17:20:31 +0530

    auth fix

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,18 +1,13 @@
 "use client";
 
 import { signIn } from "next-auth/react";
-import { FcGoogle } from "react-icons/fc";
+import React from "react";
 
-export default function SignInPage() {
+export default function signInPage() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-900">
-      <button
-        onClick={() => signIn("google")}
-        className="flex items-center bg-white text-gray-900 px-4 py-2 rounded-2xl shadow hover:shadow-lg transition-all duration-200"
-      >
-        <FcGoogle className="text-2xl mr-2" /> {/* Google logo */}
-        Sign in with Google
-      </button>
+    <div>
+      <h1>signInPage</h1>
+      <button onClick={() => signIn("google")}>SignIn with Google</button>
     </div>
   );
 }
