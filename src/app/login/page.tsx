21cc8d20ac2dfb679e45922cac8367b79389df9a
shivaacodes <shivasajay007@gmail.com>
2025-01-04 21:01:53 +0530

    added login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Card,
   CardContent,
@@ -11,23 +13,49 @@ import SubmitButton from "@/components/SubmitButtons";
 import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
 import { FaArrowLeft } from "react-icons/fa";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+// Schema validation
+const schema = z.object({
+  email: z.string().email(),
+});
+
+type FormFields = z.infer<typeof schema>;
 
 export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormFields>({
+    resolver: zodResolver(schema),
+  });
+
+  // Form submission handler
+  const onSubmit: SubmitHandler<FormFields> = async (data) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    console.log(data);
+  };
+
   return (
     <div className="relative h-screen w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
-      {/* <div className="absolute inset-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)] -z-10"></div> */}
+      {/* Back to Homepage Link */}
       <Link
         href="/"
         className={buttonVariants({
           variant: "outline",
-          className: "absolute top-5 right-5 py-2 px-4 flex items-center gap-2",
+          className: "absolute top-5 right-5 flex items-center gap-2 py-2 px-4",
         })}
       >
-        <FaArrowLeft className="size-5" /> Back to Homepage
+        <FaArrowLeft className="size-5" />
+        Back to Homepage
       </Link>
 
+      {/* Login Form */}
       <div className="flex h-full items-center justify-center px-4">
-        <Card className="font-urbanist max-w-2xl rounded-2xl">
+        <Card className="max-w-2xl rounded-2xl font-urbanist">
           <CardHeader>
             <CardTitle className="text-2xl font-bold">Login</CardTitle>
             <CardDescription>
@@ -35,19 +63,21 @@ export default function Login() {
             </CardDescription>
           </CardHeader>
           <CardContent>
-            <div className="grid w-full gap-4">
+            <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
               <div className="flex flex-col space-y-1.5">
-                <Label>Email</Label>
+                <Label htmlFor="email">Email</Label>
                 <Input
+                  {...register("email")}
                   id="email"
-                  name="email"
                   type="email"
-                  required
                   placeholder="john@example.com"
                 />
+                {errors.email && (
+                  <p className="text-red-600">{errors.email.message}</p>
+                )}
               </div>
               <SubmitButton text="Login" />
-            </div>
+            </form>
           </CardContent>
         </Card>
       </div>
